cbb69734e0ad877142f27aa253f613fc435331f4
Zx <895252509@qq.com>
2017-04-27 00:48:41 +0800

    试验了angular.cn->开发指南->显示数据

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Hero } from './app.component.model.hero';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    title = 'Tour of Heroes';
+    heroes = [
+        new Hero(1,'windstorm'),
+        new Hero(2,'Bombasto'),
+        new Hero(20,'Tornado'),
+        new Hero(11,'zx')
+    ];
+    myHero = this.heroes[0];
 }
